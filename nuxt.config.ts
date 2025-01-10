3ccdbde4950262ad3d133e9485c208c495294556
Martin Masevski <martin.masevski@gmail.com>
2025-01-10 23:46:26 +0100

    Update compatibility date in nuxt.config.ts to 2025-01-10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-
+  compatibilityDate: '2025-01-10',
   future: {
     compatibilityVersion: 4,
   },
-
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
   supabase: {
