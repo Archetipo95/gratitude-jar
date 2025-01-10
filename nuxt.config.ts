62828c1394ff0a80c7cbb5b9582178b628d61b1b
Martin Masevski <martin.masevski@gmail.com>
2025-01-11 00:53:39 +0100

    Add Supabase URL and key to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
   supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
     redirect: false,
   },
 })
