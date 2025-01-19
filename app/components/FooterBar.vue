cb5bf35d74c359dc03e30d30e77408ec130e9232
Martin Masevski <martin.masevski@gmail.com>
2025-01-19 15:48:40 +0100

    Add FooterBar component with links to info and author

---
+++ app/components/FooterBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-between items-center p-4 border-t bg-white dark:bg-gray-800 shadow-sm">
+    <p class="flex items-center gap-1">Made with <UIcon name="lucide:heart" /> by <NuxtLink to="https://github.com/Archetipo95">Martin</NuxtLink></p>
+
+    <NuxtLink to="/info" class="flex items-center gap-1">Info & Disclamers <UIcon name="lucide:badge-info" /></NuxtLink>
+  </div>
+</template>
