add80051e9ae5aefe9350722696dd6367e10fbed
Martin Masevski <martin.masevski@gmail.com>
2025-02-07 17:32:49 +0100

    Refactor ColorModeButton component to change button variant from ghost to link for improved styling

--- app/components/ColorModeButton.vue
+++ app/components/ColorModeButton.vue
@@ -13,7 +13,7 @@ const isDark = computed({
 
 <template>
   <ClientOnly v-if="!colorMode?.forced">
-    <UButton :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" color="neutral" variant="ghost" @click="isDark = !isDark" />
+    <UButton :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" color="neutral" variant="link" @click="isDark = !isDark" />
 
     <template #fallback>
       <div class="size-8" />
