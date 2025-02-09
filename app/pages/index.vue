da8a76a3e9e3bd1def87320ebdfe9c13772a010c
Martin Masevski <martin.masevski@gmail.com>
2025-02-09 20:26:01 +0100

    Show CountDown component only when user is logged in

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@ const user = useSupabaseUser()
 
 <template>
   <div class="space-y-4">
-    <CountDown v-if="!user" />
+    <CountDown v-if="user" />
     <DisclaimerMessage v-if="!user" />
 
     <WeekGrid v-if="user" />
