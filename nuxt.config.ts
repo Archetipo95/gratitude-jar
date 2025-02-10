6982df8a79ec788c37dd02e6ff9fa37806a30dcc
Martin Masevski <martin.masevski@gmail.com>
2025-02-10 09:34:08 +0100

    Add components directory configuration in Nuxt settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-10',
+
   future: {
     compatibilityVersion: 4,
   },
+
   devtools: { enabled: true },
+
   modules: ['@nuxtjs/supabase', '@nuxt/ui', '@nuxt/test-utils/module', '@nuxtjs/seo'],
+
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect: false,
   },
+
   css: ['~/assets/css/main.css'],
+
   app: {
     head: {
       htmlAttrs: { lang: 'en' },
@@ -19,15 +25,24 @@ export default defineNuxtConfig({
     pageTransition: false,
     layoutTransition: false,
   },
+
   site: {
     url: 'https://jar-of-gratitude.netlify.app',
     name: 'Gratitude Jar',
     description: 'A simple gratitude journal app built with Nuxt.js and Supabase',
     defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
   },
+
   fonts: {
     experimental: {
       processCSSVariables: true, // Enable if using CSS variables for Tailwind CSS v4
     },
   },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
 })
