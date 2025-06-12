ec9fb6cff793074712d18eca123e62838b240324
Martin Masevski <martin.masevski@gmail.com>
2025-06-12 09:53:14 +0200

    chore: update dependencies and version bump to 0.8.2; remove Stagewise toolbar integration from app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-import { VuePlugin } from "@stagewise-plugins/vue"
-import { StagewiseToolbar, type ToolbarConfig } from "@stagewise/toolbar-vue"
-
-const config: ToolbarConfig = {
-  plugins: [VuePlugin],
-}
-</script>
-
 <template>
   <UApp>
-    <ClientOnly>
-      <StagewiseToolbar :config="config" />
-    </ClientOnly>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
